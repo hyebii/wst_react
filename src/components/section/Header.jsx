a4e83aaaa7adc297ba1a5bd1ba464faa6807a673
Seong Hyeri <gofl0721@withere.com>
2023-12-14 16:48:04 +0900

    헤더 영역 데이터 작업

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,115 +1,15 @@
 import React from "react";
-import { AiFillGithub } from "react-icons/ai";
-import { AiOutlineCodepen } from "react-icons/ai";
-import { AiFillYoutube } from "react-icons/ai";
-import { AiOutlineInstagram } from "react-icons/ai";
+
+import Logo from "../header/Logo";
+import Menu from "../header/Menu";
+import Sns from "../header/Sns";
 
 const Header = () => {
   return (
     <header id="header" role="banner">
-      <h1 className="header-logo">
-        <a href="/">
-          <em aria-hidden="true"></em> {/* 애니메이션자리 */}
-          <span>
-            webs
-            <br />
-            youtube
-          </span>
-        </a>
-      </h1>
-      <nav className="header-menu">
-        <ul className="menu">
-          <li className="active">
-            <a href="/">웹스토리보이</a>
-          </li>
-          <li>
-            <a href="/today">추천영상</a>
-          </li>
-          <li>
-            <a href="/developer">추천 개발자</a>
-          </li>
-          <li>
-            <a href="/webd">웹디자인 기능사</a>
-          </li>
-          <li>
-            <a href="/website">웹표준 사이트</a>
-          </li>
-          <li>
-            <a href="/gsap">GSAP Parallax</a>
-          </li>
-          <li>
-            <a href="/port">포트폴리오 사이트</a>
-          </li>
-          <li>
-            <a href="/youtube">유튜브 클론 사이트</a>
-          </li>
-        </ul>
-        <ul className="keyword">
-          <li>
-            <a href="/search/webstoryboy">webstoryboy</a>
-          </li>
-          <li>
-            <a href="/search/html">HTML</a>
-          </li>
-          <li>
-            <a href="/search/css">CSS</a>
-          </li>
-          <li>
-            <a href="/search/javascript">JavaScript</a>
-          </li>
-          <li>
-            <a href="/search/react.js">React.js</a>
-          </li>
-          <li>
-            <a href="/search/vue.js">Vue.js</a>
-          </li>
-          <li>
-            <a href="/search/next.js">Next.js</a>
-          </li>
-          <li>
-            <a href="/search/node.js">Node.js</a>
-          </li>
-          <li>
-            <a href="/search/sql">SQL</a>
-          </li>
-          <li>
-            <a href="/search/React Portfolio">portfolio</a>
-          </li>
-          <li>
-            <a href="/search/NewJeans">music</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="header-sns">
-        <ul>
-          <li>
-            <a href="https://github.com/webstoryboy" rel="noopener noreferrer">
-              <AiFillGithub />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.youtube.com/webstoryboy"
-              rel="noopener noreferrer"
-            >
-              <AiFillYoutube />
-            </a>
-          </li>
-          <li>
-            <a href="https://codepen.io/webstoryboy" rel="noopener noreferrer">
-              <AiOutlineCodepen />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/webstoryboy"
-              rel="noopener noreferrer"
-            >
-              <AiOutlineInstagram />
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Logo />
+      <Menu />
+      <Sns />
     </header>
   );
 };
---
+++ src/data/header.js
@@ -0,0 +1,109 @@
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineCodepen } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
+export const headerMenus = [
+	{
+			title: "웹스토리보이",
+			src: "/"
+	},
+	{
+			title: "추천 영상",
+			src: "/today"
+	},
+	{
+			title: "추천 개발자",
+			src: "/developer"
+	},
+	{
+			title: "웹디자인기능사",
+			src: "/webd"
+	},
+	{
+			title: "웹표준 사이트",
+			src: "/website"
+	},
+	{
+			title: "GSAP Parallax",
+			src: "/gsap"
+	},
+	{
+			title: "포트폴리오 사이트",
+			src: "/port"
+	},
+	{
+			title: "유튜브 클론 사이트",
+			src: "/youtube"
+	},
+];
+
+export const searchKeyword = [
+	{
+			title: "webstoryboy",
+			src: "/search/webstoryboy"
+	},
+	{
+			title: "HTML",
+			src: "/search/html"
+	},
+	{
+			title: "CSS",
+			src: "/search/css"
+	},
+	{
+			title: "JavaScript",
+			src: "/search/javascript"
+	},
+	{
+			title: "React.js",
+			src: "/search/react.js"
+	},
+	{
+			title: "Vue.js",
+			src: "/search/vue.js"
+	},
+	{
+			title: "Next.js",
+			src: "/search/next.js"
+	},
+	{
+			title: "Node.js",
+			src: "/search/node.js"
+	},
+	{
+			title: "SQL",
+			src: "/search/sql"
+	},
+	{
+			title: "portfolio",
+			src: "/search/React Portfolio"
+	},
+	{
+			title: "music",
+			src: "/search/NewJeans"
+	}
+];
+
+export const snsLink = [
+	{
+			title: "github",
+			url: "https://github.com/webstoryboy",
+			icon: <AiFillGithub />
+	},
+	{
+			title: "youtube",
+			url: "https://www.youtube.com/webstoryboy",
+			icon: <AiFillYoutube />
+	},
+	{
+			title: "codepen",
+			url: "https://codepen.io/webstoryboy",
+			icon: <AiOutlineCodepen />
+	},
+	{
+			title: "instagram",
+			url: "https://www.instagram.com/webstoryboy",
+			icon: <AiOutlineInstagram />
+	},
+]
