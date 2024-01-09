86949e040ad9e491ceefbd7d8248836ab0018d62
Seong Hyeri <gofl0721@withere.com>
2024-01-09 14:08:55 +0900

    api, loading 제외 전체

--- src/pages/Channel.jsx
+++ src/pages/Channel.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import Main from '../components/section/Main'
+import { me } from '../data/me'
 
 const Channel = () => {
 	return (
 		<Main title="Channel채널" description="채널페이지입니다.">
-			Channel
+			<section id='channel'>
+                <div className='channel-header'>
+                    <div className='circle'>
+                        <img src={me[0].url} alt={me[0].title} />
+                    </div>
+                </div>
+                <div className='channel-info'>
+                    <h3 className='tit'>{me[0].title}</h3>
+                    <p className='desc'>{me[0].description}</p>
+                    <div className='info'>
+                        <span>{me[0].subscriberCount}</span>
+                        <span>{me[0].videoCount}</span>
+                        <span>{me[0].viewCount}</span>
+                    </div>
+                </div>
+			</section>
 		</Main>
 	)
 }
